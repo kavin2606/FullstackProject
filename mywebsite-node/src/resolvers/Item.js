dbc324aac0a0deb1ae2f9366aa9c92ed0493dc2b
makaur <makaur@ebay.com>
2019-05-23 13:25:21 -0700

    removing backend folder outside app

--- mywebsite-node/src/resolvers/Item.js
+++
@@ -1,7 +0,0 @@
-function postedBy(parent, args, context) {
-  return context.prisma.item({ id: parent.id }).postedBy()
-}
-
-module.exports = {
-  postedBy,
-}
--- mywebsite-node/src/resolvers/User.js
+++
@@ -1,7 +0,0 @@
-function items(parent, args, context) {
-  return context.prisma.user({ id: parent.id }).items()
-}
-
-module.exports = {
-  items,
-}
