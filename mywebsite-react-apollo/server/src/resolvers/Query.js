324ac17ada307179e00b52055c68749bbe1f01cf
kavin2606 <kavin2@pdx.edu>
2019-05-29 11:27:08 -0700

    "list all categories/filter categories and filter items within a category"

--- mywebsite-react-apollo/server/src/resolvers/Query.js
+++ mywebsite-react-apollo/server/src/resolvers/Query.js
@@ -18,9 +18,33 @@ function usersorders(parent, args, context, info) {
     id: userId,
   })
 }
+async function getCategory(parent, args, context, info) {
+  const where = args.filter ?{ category :{name: args.filter },} : {}
 
+
+  const items = await context.prisma.items({
+    where
+  })
+  return items
+}
+
+async function getCategoryItems(parent, args, context, info) {
+  const where = {
+    AND: [
+      { title_contains: args.filter },
+      { category :{name: args.filtercategory } },
+    ],
+  }
+
+  const links = await context.prisma.items({
+    where
+  })
+  return links
+}
 module.exports = {
   getItems,
-  usersorders
+  usersorders,
+  getCategory,
+  getCategoryItems,
   //userslist: (root, args, context) => context.prisma.users()
 }
