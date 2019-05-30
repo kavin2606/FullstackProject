f242977127f45efce7e1dfb0aa769bb876614671
kavin2606 <kavin2@pdx.edu>
2019-05-30 16:38:39 -0700

    "image conversion text worked"

--- ecommerce-react-apollo/src/components/App.js
+++ ecommerce-react-apollo/src/components/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import ItemList from './ItemList'
 import CreateItem from './CreateItem'
 import CreateCategory from './CreateCategory'
+import Img from './Img'
 
 class App extends Component {
   render() {
-    return <CreateItem />
+    return <Img />
   }
 }
 
