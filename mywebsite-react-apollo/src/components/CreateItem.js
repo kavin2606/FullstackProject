cc7596a350f259bcb08717b4d12a121af1a9184e
kavin2606 <kavin2@pdx.edu>
2019-05-30 19:01:41 -0700

    "created itemimg and price for items"

--- mywebsite-react-apollo/src/components/CreateItem.js
+++ mywebsite-react-apollo/src/components/CreateItem.js
@@ -4,8 +4,8 @@ import gql from 'graphql-tag'
 
 
 const CREATE_ITEM_MUTATION = gql`
-  mutation CreateItemMutation($title: String!, $description: String!, $category: String!, $quantity: String!) {
-    createItem(  title: $title, description: $description, category: $category, quantity: $quantity) {
+  mutation CreateItemMutation($title: String!, $description: String!, $category: String!, $quantity: String!,$price:String!,$itemimg:String) {
+    createItem(  title: $title, description: $description, category: $category, quantity: $quantity,price:$price,itemimg:$itemimg) {
       title
     }
   }
@@ -17,10 +17,23 @@ class CreateItem extends Component {
     description: '',
     category: '',
     quantity: '',
+    price:'',
+    itemimg:''
   }
+//   onChange(e)
+// {
+//   let files =e.target.files;
+//   let reader = new FileReader();
+//   reader.readAsDataURL(files[0]);
+//   reader.onload=(e)=>{
+//     this.setState({ itemimg: e.target.result })
+//   }
+//
+// }
+          // <input type="file" name="file" onChange={(e)=>this.onChange(e)}/>
 
   render() {
-    const { title, description, category,  quantity} = this.state
+    const { title, description, category,  quantity,price,itemimg} = this.state
     return (
       <div>
         <div className="flex flex-column mt3">
@@ -52,8 +65,22 @@ class CreateItem extends Component {
             type="text"
             placeholder="The quantity for the item"
           />
+          <input
+            className="mb2"
+            value={price}
+            onChange={e => this.setState({ price: e.target.value })}
+            type="text"
+            placeholder="The quantity for the item"
+          />
+          <input
+            className="mb2"
+            value={itemimg}
+            onChange={e => this.setState({ itemimg: e.target.value })}
+            type="text"
+            placeholder="The quantity for the image"
+          />
         </div>
-        <Mutation mutation={CREATE_ITEM_MUTATION} variables={{ title, description, quantity, category }}>
+        <Mutation mutation={CREATE_ITEM_MUTATION} variables={{ title, description, quantity, category,price,itemimg }}>
             {createItemMutation => <button onClick={createItemMutation}>Submit</button>}
         </Mutation>
       </div>
