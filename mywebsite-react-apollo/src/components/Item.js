88fdaf5baba0052e7755463a2bb89e49baf9b05d
kavin2606 <kavin2@pdx.edu>
2019-06-01 23:46:19 -0700

    "picture implementation"

--- mywebsite-react-apollo/src/components/Item.js
+++ mywebsite-react-apollo/src/components/Item.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
-
+import $ from 'jquery';
+import {reactDOMNode} from 'react-dom'
 class Item extends Component {
   render() {
-    const my_img = this.props.item.itemimg;
+
     return (
       <div>
         <div>
-          <img
-            src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+        <table><tbody>
+          <tr><td><img
+            src={this.props.item.itemimg}
             alt={this.props.item.title}
-            style={{width : '100px', height : '100px'}}
-          />
-          <p>Item Title: {this.props.item.title} </p>
+            style={{width : '160px', height : '160px'}}
+          /></td>
+          <td><p>Item Title: {this.props.item.title} </p>
           <p>Item Id: {this.props.item.id}</p>
           <p>Description: {this.props.item.description}</p>
           <p>Quantity: {this.props.item.quantity}</p>
-
-          <hr></hr>
+          <p>price: {this.props.item.price} </p></td>
+          </tr>
+          </tbody></table>
         </div>
       </div>
     )
