43a7fec979bef694e37d2867ed4d5fde3152561a
kavin2606 <kavin2@pdx.edu>
2019-06-01 18:09:18 -0700

    "placeholder image update"

--- mywebsite-react-apollo/src/components/Item.js
+++ mywebsite-react-apollo/src/components/Item.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 
 class Item extends Component {
   render() {
+    const my_img = this.props.item.itemimg;
     return (
       <div>
         <div>
-            <p>Item Title: {this.props.item.title} </p>
-            <p>Item Id: {this.props.item.id}</p>
-            <p>Description: ({this.props.item.description})</p>
-            <p>Quantity: {this.props.item.quantity}</p>
+          <img
+            src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+            alt={this.props.item.title}
+            style={{width : '100px', height : '100px'}}
+          />
+          <p>Item Title: {this.props.item.title} </p>
+          <p>Item Id: {this.props.item.id}</p>
+          <p>Description: {this.props.item.description}</p>
+          <p>Quantity: {this.props.item.quantity}</p>
+
           <hr></hr>
         </div>
       </div>
