c4c97255d2a6f81a4474016d618c4bd64a8a3bf0
makaur <makaur@ebay.com>
2019-06-02 19:33:00 -0700

    item fields updated to look same across all forms

--- mywebsite-react-apollo/src/components/Item.js
+++ mywebsite-react-apollo/src/components/Item.js
@@ -3,13 +3,13 @@ import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 
 const PLACE_ORDER_MUTATION = gql`
-  mutation PlaceOrderMutation($id: String!)
+mutation PlaceOrderMutation($id: String!)
+{
+  placeorder(id: $id)
   {
-    placeorder(id: $id)
-    {
-      id
-    }
+    id
   }
+}
 `
 class Item extends Component {
   render() {
@@ -17,26 +17,30 @@ class Item extends Component {
     return (
       <div>
         <div>
-        <table><tbody>
-          <tr><td style={{width:200}} ><img
-            src={this.props.item.itemimg}
-            alt={this.props.item.title}
-            style={{width : '160px', height : '160px'}}
-          /></td>
-          <td style={{width:500}}><p>Item Title: {this.props.item.title} </p>
-          <p>Description: {this.props.item.description}</p>
-          <p>Quantity: {this.props.item.quantity}</p>
-          <p>price: {this.props.item.price} </p>
-          <p>Category: {this.props.item.category.name}</p></td>
-
-          <td>
+          <table>
+            <tbody>
+              <tr>
+                <td style={{width:250}}>
+                  <img src={this.props.item.itemimg} alt={this.props.item.title} style={{width : '200px', height : '160px'}}/>
+                </td>
+                <td style={{width:700}}>
+                  <p><b>Item:</b> {this.props.item.title} </p>
+                  <p><b>Description:</b> {this.props.item.description}</p>
+                  <p><b>Price:</b> {this.props.item.price} </p>
+                  <p><b>Quantity:</b> {this.props.item.quantity}</p>
+                  <p><b>Category:</b> {this.props.item.category.name}</p>
+                  <p><b>Item Id:</b> {this.props.item.category.id}</p>
+                </td>
+                <td>
                   <Mutation mutation={PLACE_ORDER_MUTATION} variables={{id}}>
-                      {placeOrderMutation => <button onClick={placeOrderMutation}>Buy now</button>}
+                  {placeOrderMutation => <button onClick={placeOrderMutation}>Buy now</button>}
                   </Mutation>
-                  </td>
-          </tr>
-          </tbody></table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+        <hr></hr>
       </div>
     )
   }
